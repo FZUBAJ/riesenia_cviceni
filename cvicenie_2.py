14bb8b44f09199d0676a85f75a71045a63f7471e
FZUBAJ <93700761+FZUBAJ@users.noreply.github.com>
2022-01-08 16:17:27 +0100

    Add files via upload

---
+++ cvicenie_2.py
@@ -0,0 +1,9 @@
+meno = input("Ako sa voláš?")
+vek = int(input("Koľko máš rokov?"))
+
+print("Voláš sa " + meno + " a máš " + str(vek) + " rokov.")
+
+o_rok_starsi = vek + 1
+o_10_rokov_starsi = vek + 10
+
+print("O rok budeš mať " + str(o_rok_starsi) + " rokov a o 10 rokov budeš mať " + str(o_10_rokov_starsi) + " rokov.")
---
+++ cvicenie_3.py
@@ -0,0 +1,5 @@
+a = 10
+obvod = 4 * a
+obsah= a * a
+
+print("Obvod štvorca je " + str(obvod) + " cm a jeho obsah je " + (str(obsah)) + " cm.")
